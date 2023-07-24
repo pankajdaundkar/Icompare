f53927d8de8e8518eacea46b1399e7c0e10e5ec1
pankajdaundkar <phd27993@gmail.com>
2023-07-24 13:40:27 +0530

    Add project files.

---
+++ Icopare/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icopare
+{
+    public class Employee
+    {
+        public int Salary { get; set; }
+        public string Name { get; set; }
+
+
+    }
+    public class SalaryComparison : IComparer<Employee>
+    {
+       public  int Compare(Employee x, Employee y)
+        {
+            if (x.Salary < y.Salary)
+            {
+                return -1;
+
+            }
+            else if (x.Salary > y.Salary)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+       
+    }
+}
---
+++ Icopare/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icopare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee {Salary= 10000,Name="pankaj" };
+            Employee employee2 = new Employee {Salary = 20000,Name="pratik" };
+
+            SalaryComparison sal = new SalaryComparison();
+            int result = sal.Compare(employee, employee2);
+
+            if (result < 0)
+            {
+                Console.WriteLine("Pnakaj salary is less than Pratik");
+            }
+            else if (result > 0)
+            {
+                Console.WriteLine("Pankaj salary is more than Pratik");
+            }
+            else
+            {
+                Console.WriteLine("salary of Pankaj is same as Pratik");
+            }
+            
+
+        }
+    }
+}
